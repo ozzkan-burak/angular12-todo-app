ab859583bbcc8cacafd149e4d2f066e5344d61db
Burak Özkan <burak.ozkan@path.com.tr>
2021-10-14 01:21:49 +0300

    create todo interface

--- todo-app/src/app/todo/todo.component.ts
+++ todo-app/src/app/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoItem } from '../todoItem';
 
 @Component({
   selector: 'app-todo',
@@ -9,17 +10,20 @@ export class TodoComponent {
 
   constructor() { }
 
-  private name= "Burak";
+  private name: string = "Burak";
 
-  // items = [
-  //   "item 1", "item 2", "item 3"
+  // items: TodoItem[] = [
+  //   new TodoItem("kahvaltı", "yes"),
+  //   new TodoItem("spor", "yes"),
+  //   new TodoItem("alışveriş", "no")
   // ]
 
-  items = [
+  items: TodoItem[] = [
     {description: "kahvaltı", status: "yes"},
     {description: "spor", status: "yes"},
-    {description: "alışveriş", status: "no"}
+    {description: "alışveriş", status: "no"},
   ]
+
 
   getName() {
     return this.name
